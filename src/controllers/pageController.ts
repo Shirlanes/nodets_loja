329b73222d7bf72562c84c04404724e3de926f0d
Shirlanes <rshirlane216@gmail.com>
2022-09-19 08:27:21 -0300

    ajustando menu e filtro

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
+import { createMenuObject } from "../helpers/createMenuObject";
 
 export const home = (req: Request, res: Response) => {
     res.render("pages/page", {
+        menu: createMenuObject('all'),
         banner: {
             title: "Todos os produtos",
         background: "banner.jpg",
@@ -11,6 +13,7 @@ export const home = (req: Request, res: Response) => {
 
 export const vestidos = (req: Request, res: Response) => {
     res.render("pages/page", {
+         menu: createMenuObject('vestido'),
         banner: {
             title: "Vestidos",
         background: "bannerVestidos.jpeg",
@@ -20,6 +23,7 @@ export const vestidos = (req: Request, res: Response) => {
 
 export const sapatos = (req: Request, res: Response) => {
      res.render("pages/page", {
+         menu: createMenuObject('sapato'),
         banner: {
             title: "Sapatos",
         background: "bannerSapatos.jpeg",
@@ -29,6 +33,7 @@ export const sapatos = (req: Request, res: Response) => {
 
 export const joias = (req: Request, res: Response) => {
      res.render("pages/page", {
+         menu: createMenuObject('joia'),
         banner: {
             title: "Joias",
         background: "bannerJoias.jpeg",
